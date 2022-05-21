c542ab1f8d556c8d9ccdb65d510665e23aa3be86
giamazonas <skibinskirenee@gmail.com>
2022-05-21 11:58:49 -0400

    add to gh

--- src/pages/OrgList/OrgList.jsx
+++ src/pages/OrgList/OrgList.jsx
@@ -25,12 +25,12 @@ const OrgList = (props) => {
       className="ListContainer" 
       ><br /><br />
         <h1>GitHub Organizations</h1>
-        {items.slice(0, count).map(item => (
+        {/* {items.slice(0, count).map(item => (
           <OrgCard
             key={item._id} 
             item={item} 
           />
-        ))}
+        ))} */}
         <button className="seeMoreButton" onClick={addTen}>See 10 More</button>
       </div>
     </div>
